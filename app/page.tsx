8124e90971254cfb851d9f6e8eba688c60433124
Nika3406 <nikashvely15@gmail.com>
2025-07-15 09:52:36 -0400

    last update

--- app/page.tsx
+++ app/page.tsx
@@ -35,7 +35,7 @@ export default function Home() {
         </a>
 
         <a
-          href="https://www.youtube.com/@KoolKouch"
+          href="https://www.youtube.com/watch?v=j70qRsc4JMA"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
           target="_blank"
           rel="noopener noreferrer"
